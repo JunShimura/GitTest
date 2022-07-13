504a578099c79b5b0d0c359f467407673c6ad05b
Jun Shimura <j.shimura@sugawara.ac.jp>
2022-07-13 16:26:25 +0900

    System.random

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
+    Rigidbody rb;
+    System.Random r;
+    float t=0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //rb =gameObject.AddComponent<Rigidbody>();
+        //rb.isKinematic = true;
+        r = new System.Random();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, 0.125f * Time.deltaTime, 0);
+        t+=Time.deltaTime;
+        var u = r.Next(-30, 31);
+        transform.position=new Vector3(0, u* Time.deltaTime*Mathf.Sin(t)*10, 0);
     }
 }
